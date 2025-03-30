c6d3bb5bde58b6fdeeab0972348a3bbb70f7b2de
WilsonJWong <wilson.jw.wong@gmail.com>
2025-03-30 13:04:58 +1100

    Week 5 Activity 1 - Task 1

--- src/main.js
+++ src/main.js
@@ -7,21 +7,6 @@ import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 
-import { initializeApp } from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyD-oYRgTsDcsxfbHnZZXM8scO1fsqBp2Gs',
-  authDomain: 'week-4---wilson.firebaseapp.com',
-  projectId: 'week-4---wilson',
-  storageBucket: 'week-4---wilson.firebasestorage.app',
-  messagingSenderId: '866195190393',
-  appId: '1:866195190393:web:63f08adb50fc14057a1aa8',
-}
-
-initializeApp(firebaseConfig)
-
 const app = createApp(App)
 app.use(PrimeVue, { theme: { preset: Aura } })
 app.use(router)
