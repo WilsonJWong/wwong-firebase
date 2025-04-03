da3d982443b10bab6889b627452547699f57aa2d
WilsonJWong <wilson.jw.wong@gmail.com>
2025-04-03 17:11:19 +1100

    Checkpoint

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -15,6 +15,11 @@
         <li class="nav-item">
           <router-link to="/addbook" class="nav-link" active-class="active">AddBook</router-link>
         </li>
+        <li class="nav-item">
+          <router-link to="/GetBookCount" class="nav-link" active-class="active"
+            >Get Book Count</router-link
+          >
+        </li>
       </ul>
     </header>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import FireView from '../views/FirebaseSigninView.vue'
+import FireLoginView from '../views/FirebaseSigninView.vue'
 import FireRegisterView from '../views/FirebaseRegisterView.vue'
-import AddBookView from '@/views/AddBookView.vue'
+import FireLogoutView from '../views/FirebaseLogoutView.vue'
+import GetBookCountView from '../views/GetBookCountView.vue'
 
 const routes = [
   {
     path: '/fireLogin',
     name: 'FireLogin',
-    component: FireView,
+    component: FireLoginView,
   },
   {
     path: '/fireRegister',
@@ -15,9 +16,14 @@ const routes = [
     component: FireRegisterView,
   },
   {
-    path: '/addBook',
-    name: 'AddBook',
-    component: AddBookView,
+    path: '/fireLogout',
+    name: 'FireLogout',
+    component: FireLogoutView,
+  },
+  {
+    path: '/GetBookCount',
+    name: 'GetBookCount',
+    component: GetBookCountView,
   },
 ]
 
